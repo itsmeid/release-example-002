dca7983b9c387172b31fda30867e811f5f514f62
id <0x6964@gmail.com>
2024-12-20 17:07:50 +0700

    chore: initial

---
+++ __test__/src/module-a.spec.ts
@@ -0,0 +1,19 @@
+import { describe, expect, it } from 'bun:test';
+import { joinString } from 'src/module-a';
+
+describe('module-a', () => {
+  it('joinString', () => {
+    expect(joinString('a', 'b')).toBe('ab');
+  });
+
+  it('longer duration', () => {
+    const temp = [];
+    const length = 1000000;
+
+    for (let i = 0; i < length; i++) {
+      temp.push(joinString('a', 'b'));
+    }
+
+    expect(temp).toBeArrayOfSize(length);
+  });
+});
