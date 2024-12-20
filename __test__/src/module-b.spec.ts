d7a8166f6e8ec5a23583cc5f0486beebc18b7112
id <0x6964@gmail.com>
2024-12-20 17:07:50 +0700

    chore: initial

---
+++ __test__/src/module-b.spec.ts
@@ -0,0 +1,19 @@
+import { describe, expect, it } from 'bun:test';
+import { joinObject } from 'src/module-b';
+
+describe('module-b', () => {
+  it('joinObject', () => {
+    expect(joinObject({ a: 'a' }, { b: 'b' })).toEqual({ a: 'a', b: 'b' });
+  });
+
+  it('longer duration', () => {
+    const temp = [];
+    const length = 1000000;
+
+    for (let i = 0; i < length; i++) {
+      temp.push(joinObject({ a: 'a' }, { b: 'b' }));
+    }
+
+    expect(temp).toBeArrayOfSize(length);
+  });
+});
