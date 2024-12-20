6e7ae627fc693a3526aaa41eb8db32606b2b2474
id <0x6964@gmail.com>
2024-12-20 17:07:50 +0700

    chore: initial

---
+++ commitlint.config.ts
@@ -0,0 +1,26 @@
+import type { UserConfig } from '@commitlint/types';
+
+const Config: UserConfig = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test'
+      ]
+    ]
+  }
+};
+
+export default Config;
