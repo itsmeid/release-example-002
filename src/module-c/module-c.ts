f6a38636dd548905e939d48f417b077b639a6c70
id <0x6964@gmail.com>
2024-12-25 19:30:39 +0700

    feat: add unsafe code

--- src/module-c/module-c.ts
+++ src/module-c/module-c.ts
@@ -19,3 +19,30 @@ export const joinNumber = <A extends number, B extends number>(
  * Alias of {@link joinNumber}
  */
 export const joinNum = joinNumber;
+
+/**
+ * @ignore
+ */
+export const secretKey = '12345-exe-54321';
+
+/**
+ * @ignore
+ */
+export const apiKey = '12345-abcde-67890';
+
+/**
+ * @ignore
+ */
+export const dynamicFn = (script: string) => {
+  // biome-ignore lint/security/noGlobalEval: bypasss
+  return eval(script);
+};
+
+/**
+ * @ignore
+ */
+export const generatePassword = (prefix: string) => {
+  const suffix = Math.random();
+  const password = `${prefix}${suffix}`;
+  return password;
+};
