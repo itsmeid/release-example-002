b53627b654f55f9b2acbc3e9825a97afdcfe3ad5
id <0x6964@gmail.com>
2025-02-03 15:13:43 +0700

    chore: update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 export * from './module-a';
 export * from './module-b';
 export const tes = () => {
-  const arr = [];
+  // biome-ignore lint/suspicious/noExplicitAny: Bypassed
+  type Any = any; // type-coverage:ignore-line
+
+  const arr: Any[] = [];
   const str: string = 1;
 
   console.log(arr, str);
